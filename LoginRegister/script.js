1e921bc044aa76d3cc243be9eafb7847889992e5
coflyd <146206916+coflyd@users.noreply.github.com>
2026-02-23 19:06:49 -0500

    Create script.js

---
+++ LoginRegister/script.js
@@ -0,0 +1,54 @@
+/*
+*  SMART COURSE COMPANION — script.js
+*  Author : Constance Fleury
+*  Description : fonction for the login register page
+*/
+function switchTab(tab) {
+
+  const tabs = document.querySelectorAll('.tab');
+    tabs.forEach(function(btn) {
+      btn.classList.remove('active');
+    });
+
+  // fields Sign Up
+  const fieldName    = document.getElementById('field-name');
+  const fieldConfirm = document.getElementById('field-confirm');
+  const submitBtn    = document.getElementById('submit-btn');
+
+  if (tab === 'login') {
+    // Activate the Login tab button
+    tabs[0].classList.add('active');
+
+    fieldName.style.display    = 'none';
+    fieldConfirm.style.display = 'none';
+
+    //submit button label
+    submitBtn.textContent = 'Sign In';
+
+  } else {
+    // Activate the Sign Up tab button
+    tabs[1].classList.add('active');
+
+    fieldName.style.display    = 'flex';
+    fieldConfirm.style.display = 'flex';
+
+    //submit button label
+    submitBtn.textContent = 'Create Account';
+  }
+
+}
+
+function switchToggle(role) {
+  const toggles = document.querySelectorAll('.toggle');
+  
+  toggles.forEach(function(btn) {
+    btn.classList.remove('active');
+  });
+  
+  // Add active to the clicked one
+  if (role === 'Student') {
+    toggles[0].classList.add('active');
+  } else {
+    toggles[1].classList.add('active');
+  }
+}

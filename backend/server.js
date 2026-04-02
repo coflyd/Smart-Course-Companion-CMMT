e6c89f3b773898a5685fd044db5298bddb6b62e5
coflyd <cfleury@et.esiea.fr>
2026-04-02 14:29:13 -0400

    add register route

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
+//add
 const db = require('./db');
 
 const express = require('express'); // importes la librairie Express
 const app = express(); //crées ton application serveur
 PORT = 3000; //port
 
+const authRoutes= require('./routes/auth'); 
+
 //le serveur quand reçois une requête, lis le JSON dedans
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => { //route (request, response)
     res.json({ message: 'Server is awake' });

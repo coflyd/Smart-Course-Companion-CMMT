f64011e524e0c12d102fe3f39f17a4220c77eb0e
coflyd <cfleury@et.esiea.fr>
2026-04-02 12:26:58 -0400

    backend with Express server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express'); // importes la librairie Express
+const app = express(); //crées ton application serveur
+PORT = 3000; //port
+
+//le serveur quand reçois une requête, lis le JSON dedans
+app.use(express.json());
+
+app.get('/', (req, res) => { //route (request, response)
+    res.json({ message: 'Server is awake' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

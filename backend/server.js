1356079b33b138a35bf67fa29db7691243b57b0e
coflyd <146206916+coflyd@users.noreply.github.com>
2026-04-03 19:54:40 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
-//add
+/*
+SMART COURSE COMPANION — server.js
+Auteur : Constance Fleury - 40348933
+Description : Creation of the server  : Port 3000 → serveur Node.js
+*/
+
+//added later in the dev 
 const db = require('./db');
 
-const express = require('express'); // importes la librairie Express
-const app = express(); //crées ton application serveur
+const express = require('express'); // import the librairy Express
+const app = express(); // application serveur
 PORT = 3000; //port
 
 const authRoutes= require('./routes/auth'); 
 
-//le serveur quand reçois une requête, lis le JSON dedans
+//When the server receives a request, it reads the JSON contained within it
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 

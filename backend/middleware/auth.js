062dd8e746eedac226c60aee4fd14b1c572762d2
Thushi-T <thushika1994@gmail.com>
2026-04-03 22:29:09 -0400

    Add JWT middleware for protected routes

---
+++ backend/middleware/auth.js
@@ -0,0 +1,25 @@
+/* Thushika Thavarajah (27516126)*/
+
+
+//Step 1. Using jwt why? because it is easier to connect the frontend and the backend
+const jwt = require('jsonwebtoken');
+
+//Step 2. Create a function protect that runs on every protected route. 
+const protect = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    //If token does not exist, shall not pass!!!
+    if (!token) {
+        return res.status(401).json({ error: 'Not logged in' });
+    }
+    //If toke is not valid, shall not pass
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ error: 'Token is invalid' });
+    }
+};
+
+module.exports = protect;

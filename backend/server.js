c66fecde53b41d51725194a6ef0eb5e42abe0ab8
coflyd <146206916+coflyd@users.noreply.github.com>
2026-04-04 16:27:57 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,33 +4,25 @@ Auteur : Constance Fleury - 40348933
 Description : Creation of the server  : Port 3000 → serveur Node.js
 */
 
-//added later in the dev 
-const db = require('./db');
-
 const express = require('express'); // import the librairy Express
 const app = express(); // application serveur
-PORT = 3000; //port
+const PORT = 3000; //port
+const db = require('./db');
 
 //When the server receives a request, it reads the JSON contained within it
 app.use(express.json());
 
-const authRoutes= require('./routes/auth'); 
-app.use('/api/auth', authRoutes);
-
-//Courses
+const authRoutes = require('./routes/auth');
 const courseRoutes = require('./routes/courses');
-app.use('/api/courses', courseRoutes);
-
-//Assignment
 const assignmentRoutes = require('./routes/assignments');
-app.use('/api/assignments', assignmentRoutes);
-
-//Grades
 const gradesRoutes = require('./routes/grades');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/courses', courseRoutes);
+app.use('/api/assignments', assignmentRoutes);
 app.use('/api/grades', gradesRoutes);
 
-
-app.get('/', (req, res) => { //route (request, response)
+app.get('/', (req, res) => {
     res.json({ message: 'Server is awake' });
 });
 

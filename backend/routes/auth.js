7bc2946fa297f8f0d9fcf40c2964f3d7f7d82dda
coflyd <146206916+coflyd@users.noreply.github.com>
2026-04-03 22:55:35 -0400

    Update auth.js
    Add the /login route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,6 +8,8 @@ const router = express.Router();
 const bcrypt= require('bcrypt'); 
 const db = require('../db'); 
 
+const jwt = require('jsonwebtoken');
+
 router.post('/register', async (req, res) => {
     const { name, email, password, role } = req.body; 
 
@@ -20,6 +22,32 @@ router.post('/register', async (req, res) => {
     await db.query(sql, [name, email, hashedPassword, role]);
     res.status(201).json({ message: 'User created successfully' });
 
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body; 
+
+    if (!email || !password){
+        return res.status(400).json({ error : 'All fields are required' })
+    }
+
+    const sql = 'SELECT * FROM users WHERE email = ?';
+    const [rows] = await db.query(sql, [email]);
+    const user = rows[0]; 
+
+    if(!user){
+         return res.status(400).json({ error : 'User not found' })
+    }
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+        return res.status(401).json({ error: 'Wrong password' });
+    }
+    const token = jwt.sign(
+    { id: user.id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: '5h' }
+    );
+    res.status(200).json({ token, role: user.role });
 });
 
 module.exports = router;

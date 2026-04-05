dddc4d00fd847260e8488c80ffe3b44ca72e4129
coflyd <146206916+coflyd@users.noreply.github.com>
2026-04-05 19:16:07 -0400

    Update grades.js

--- backend/routes/grades.js
+++ backend/routes/grades.js
@@ -31,10 +31,16 @@ router.get('/me', protect, async (req, res) => {
 
 // POST grades (instructor)
 router.post('/', protect, async (req, res) => {
-    const { assessments_id, student_id, status, earned_marks, total_marks } = req.body;
-    const sql = 'INSERT INTO grades (assessments_id, student_id, status, earned_marks, total_marks) VALUES (?, ?, ?, ?, ?)';
-    await db.query(sql, [assessments_id, student_id, status, earned_marks, total_marks]);
-    res.status(201).json({ message: 'Grade posted' });
+    const { assessments_id, status, earned_marks, total_marks } = req.body;
+    const { id } = req.user;
+    const sql = `INSERT INTO grades (assessments_id, student_id, status, earned_marks, total_marks) 
+                 VALUES (?, ?, ?, ?, ?)
+                 ON DUPLICATE KEY UPDATE
+                 earned_marks = VALUES(earned_marks),
+                 total_marks = VALUES(total_marks),
+                 status = VALUES(status)`;
+    await db.query(sql, [assessments_id, id, status, earned_marks, total_marks]);
+    res.status(201).json({ message: 'Grade saved' });
 });
 
 module.exports = router;

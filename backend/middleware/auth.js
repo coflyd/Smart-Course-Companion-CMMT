c89efd0fc5ba6f4ed30e6a79ac3d0c6a9a838ba2
Thushi-T <thushika1994@gmail.com>
2026-04-05 00:45:46 -0400

    Login, Protected Routes, JWT, all tested with Thunder Client, changed db_name and added port

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,16 +6,16 @@ const jwt = require('jsonwebtoken');
 
 //Step 2. Create a function protect that runs on every protected route. 
 const protect = (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1];
+const token = req.headers.authorization?.split(' ')[1];
 
     //If token does not exist, shall not pass!!!
     if (!token) {
-        return res.status(401).json({ error: 'Not logged in' });
+    return res.status(401).json({ error: 'Not logged in' });
     }
-    //If toke is not valid, shall not pass
+    //If token is not valid, shall not pass
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decode;
         next();
     } catch (err) {
         res.status(401).json({ error: 'Token is invalid' });

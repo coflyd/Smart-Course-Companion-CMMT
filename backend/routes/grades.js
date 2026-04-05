67b7206cd051006ea94dad6cf62fa241e77de05a
coflyd <146206916+coflyd@users.noreply.github.com>
2026-04-05 16:28:28 -0400

    Update grades.js
    Add the Get the grades of the student by Id in URL

--- backend/routes/grades.js
+++ backend/routes/grades.js
@@ -18,6 +18,17 @@ router.get('/student/:student_id', protect, async (req, res) => {
     res.status(200).json(rows);
 });
 
+//Get the grades of the student by Id in URL
+router.get('/me', protect, async (req, res) => {
+    const { id } = req.user;
+    const sql = `SELECT grades.*, assessments.assessment_title, assessments.weight, assessments.course_code
+                 FROM grades 
+                 JOIN assessments ON grades.assessments_id = assessments.id 
+                 WHERE grades.student_id = ?`;
+    const [rows] = await db.query(sql, [id]);
+    res.status(200).json(rows);
+});
+
 // POST grades (instructor)
 router.post('/', protect, async (req, res) => {
     const { assessments_id, student_id, status, earned_marks, total_marks } = req.body;
